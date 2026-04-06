06563a3c2310eaea23cd3abfa18dd6b1c26ba5d1
artemkhaha@mail.ru <artemkhaha@mail.ru>
2026-04-06 11:11:10 +0700

    Обновил код треугольника в main

--- code.cpp
+++ code.cpp
@@ -7,16 +7,24 @@ int main() {
     cout << "Введите стороны: ";
     cin >> a >> b >> c;
     
-    cout << "P = " << a + b + c << endl;
+    if (a <= 0 || b <= 0 || c <= 0) {
+        cout << "Ошибка: Стороны должны быть положительными!" << endl;
+        return 1;
+    }
     
-    double p = (a + b + c) / 2;
-    cout << "S = " << sqrt(p * (p - a) * (p - b) * (p - c)) << endl;
-    
-    // Равнобедренный = ровно две стороны равны, а третья отличается
-    if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
-        cout << "Равнобедренный" << endl;
-    else
-        cout << "Не равнобедренный" << endl;
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << "P = " << a + b + c << endl;
+        double p = (a + b + c) / 2;
+        cout << "S = " << sqrt(p * (p - a) * (p - b) * (p - c)) << endl;
+        
+        if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
+            cout << "Равнобедренный" << endl;
+        else
+            cout << "Не равнобедренный" << endl;
+    }
+    else {
+        cout << "Ошибка: Треугольник не существует!" << endl;
+    }
     
     return 0;
 }
