eebbdc4a619cea3b8986f736db82a9d3ff6b8d61
artemkhaha@mail.ru <artemkhaha@mail.ru>
2026-04-09 14:53:45 +0700

    asd

--- code.cpp
+++ code.cpp
@@ -6,17 +6,14 @@ int main() {
     double a, b, c;
     cout << "Введите стороны: ";
     cin >> a >> b >> c;
-    
     if (a <= 0 || b <= 0 || c <= 0) {
         cout << "Ошибка: Стороны должны быть положительными!" << endl;
         return 1;
     }
-    
     if (a + b > c && a + c > b && b + c > a) {
         cout << "P = " << a + b + c << endl;
         double p = (a + b + c) / 2;
         cout << "S = " << sqrt(p * (p - a) * (p - b) * (p - c)) << endl;
-        
         if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
             cout << "Равнобедренный" << endl;
         else
@@ -25,7 +22,6 @@ int main() {
     else {
         cout << "Ошибка: Треугольник не существует!" << endl;
     }
-    
     return 0;
 }
 
